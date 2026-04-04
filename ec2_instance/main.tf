e36ed928c7c0a14aa7850e4c1f0d0653b2fa7f31
Ankitanki007 <81515919+Ankitanki007@users.noreply.github.com>
2026-04-04 13:21:38 +0530

    Remove AWS provider and instance tags from main.tf
    Removed AWS provider configuration and instance tags.

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,16 +1,4 @@
-provider "aws" {
-  region = "us-west-1"
-}
-
-
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-<<<<<<< HEAD
-=======
-  key_name      = "ec2" 
->>>>>>> 5f578f4371ebbf1d8d987aca013988b71d80363a
-  tags = {
-    Name = var.instance_name
-  }
-}
+
